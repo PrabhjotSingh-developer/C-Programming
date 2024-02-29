a9df03fa8468ad9ab496ffc24c6654eb51bf23be
Prabhjot Singh <prabhjotsingh1032003@gmail.com>
2024-02-29 13:49:51 +0530

    input and output question complete

---
+++ 1. Input and output in C Language/Ques3.c
@@ -0,0 +1,7 @@
+// 3. Write a program to print “Coding” on the screen. (Remember to print in double quotes)
+
+#include <stdio.h>
+int main()
+{
+     printf("\"Coding\"");
+}
---
+++ 1. Input and output in C Language/Ques5.c
@@ -0,0 +1,8 @@
+// 5. WAP to calculate the length of String using printf function
+#include <stdio.h>
+int main()
+{
+    int length = printf("hello");
+    printf("\n");
+    printf("%d",length);
+}
